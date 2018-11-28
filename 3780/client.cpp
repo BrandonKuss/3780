ee849ed400819be03030bbbf88310281136614f0
btk-dev <brandon.kuss@mymhc.ca>
2018-11-27 17:02:14 -0700

    Add files via upload
    Some changes to server and client.cpp as well as added text file.

--- 3780/client.cpp
+++ 3780/client.cpp
@@ -2,6 +2,7 @@
 #include "SocketException.h"
 #include <iostream>
 #include <string>
+#include <bits/stdc++.h>
 
 int main(int argc, int argv[])
 {
@@ -10,11 +11,33 @@ int main(int argc, int argv[])
       // that you're running your server.
       ClientSocket client_socket("localhost", 30000);
       std::string reply;
+      std::string formattedReply;
       // Usually in real applications, the following
-      // will be put into a loop. 
+      // will be put into a loop.
       try {
-	 client_socket << "Test message.";
+	 //client_socket << "Test message.";
 	 client_socket >> reply;
+	 int n = reply.length();
+	 std::vector<char> incoming;
+	 char parity;
+	 std::string ackNumber;
+	 /*
+	 char recieved_array[n+1];
+	 strcpy(recieved_array, reply.c_str());
+	 std::vector<char> incoming(std::begin(recieved_array), std::end(recieved_array));
+	 char parity = incoming.pop_back();
+	 std::string ackNumber = incoming.pop_back();
+	 */
+     /*
+	 for(int i = 0; i < reply.length(); i++) {
+        if (i = reply.length())
+            ackNumber = reply[i];
+        if (i = reply.length() - 1)
+            parity = reply[i];
+        incoming.push_back(reply[i]);
+	 }
+	 */
+	 client_socket << reply;
       }
       catch(SocketException&){
       }
--- 3780/server.cpp
+++ 3780/server.cpp
@@ -1,10 +1,33 @@
 #include "ServerSocket.h"
 #include "SocketException.h"
 #include <string>
- 
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <bits/stdc++.h>
+
+int checkParity(std::string incoming) {
+    std::string parityCheck;
+    parityCheck = incoming;
+    int currentCount = 0, temp;
+    for (int i = 0; i < parityCheck.length(); i++) {
+        char tmp = parityCheck[i];
+        int translation = static_cast<int>(tmp);
+        currentCount += translation;
+    }
+    while(currentCount != 0 ) {
+        temp = currentCount % 2;
+        currentCount /= 2;
+    }
+    if ((temp % 2) == 0)
+        return 1;
+    else
+        return 0;
+}
+
 int main(int argc, int argv[])
 {
-   std::cout << "running....\n";
+   std::cout << "running..\n";
    try{
       // Create the socket
       ServerSocket server(30000);
@@ -12,6 +35,22 @@ int main(int argc, int argv[])
       while (true){
 	 ServerSocket new_sock;
 	 server.accept(new_sock);
+	 /*
+     ServerSocket data_sock;
+     server.accept(data_sock);
+     */
+
+     std::vector<std::string> toSend;
+     std::string currentLine;
+     std::ifstream infile;
+     infile.open("s1.txt");
+     while(std::getline(infile, currentLine, '.')) {
+        toSend.push_back(currentLine);
+     }
+     bool canSend = true;
+     int counter = 0;
+     int parity_bit;
+
 
 	 // For multiple threading, you need to create
 	 // a new thread here and pass new_sock to it.
@@ -20,8 +59,22 @@ int main(int argc, int argv[])
 	 try{
 	    while (true){
 	       std::string data;
+	       if (canSend) {
+            std::string sending = toSend[counter];
+            sending.append(std::to_string(counter));
+            parity_bit = checkParity(toSend[counter]);
+            sending.append(std::to_string(parity_bit));
+            new_sock << sending;
+	       }
 	       new_sock >> data;
-	       new_sock << data;
+	       int n = data.length();
+	       char incoming_array[n+1];
+	       strcpy(incoming_array, data.c_str());
+	       if (incoming_array[n - 2] == counter) {
+                canSend = true;
+                counter ++;
+	       };
+	       //new_sock << data;
 	    }
 	 }
 	 catch(SocketException&){
