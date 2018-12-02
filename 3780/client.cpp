5f5dd8618c2ce7b9c8b40922ef0fcf26c62e4c62
btk-dev <brandon.kuss@mymhc.ca>
2018-12-02 14:40:00 -0700

    Add files via upload
    Latest, desperate changes

--- 3780/client.cpp
+++ 3780/client.cpp
@@ -4,16 +4,19 @@
 #include <string>
 #include <bits/stdc++.h>
 
-int checkParity(std::string incoming) {
+int checkParity(std::string incoming)
+{
     std::string parityCheck;
     parityCheck = incoming;
     int currentCount = 0, temp;
-    for (int i = 0; i < parityCheck.length(); i++) {
+    for (int i = 0; i < parityCheck.length(); i++)
+    {
         char tmp = parityCheck[i];
         int translation = static_cast<int>(tmp);
         currentCount += translation;
     }
-    while(currentCount != 0 ) {
+    while(currentCount != 0 )
+    {
         temp = currentCount % 2;
         currentCount /= 2;
     }
@@ -25,41 +28,53 @@ int checkParity(std::string incoming) {
 
 int main(int argc, int argv[])
 {
-   try{
-      // Replace "localhost" with the hostname
-      // that you're running your server.
-      ClientSocket client_socket("localhost", 30000);
-      std::string reply;
-      std::string formattedReply;
-      // Usually in real applications, the following
-      // will be put into a loop.
-      try {
-	 //client_socket << "Test message.";
-	 client_socket >> reply;
-	 int n = reply.length();
-	 std::string incoming;
-	 std::string parity;
-	 std::string ackNumber;
-	 incoming = reply.substr(0, n-2);
-	 formattedReply = incoming;
-	 parity = reply.substr(n-2, 1);
-	 ackNumber = reply.substr(n-1,1);
-	 int parityCheck = checkParity(formattedReply);
-	 if (parityCheck = parity) {
-        client_socket << ackNumber;
-	 }
-	 else {
-        client_socket << "";
-	 }
-	 //client_socket << ackNumber;
-      }
-      catch(SocketException&){
-      }
-      std::cout << "We received this response from the server:\n\"" << formattedReply << "\"\n";;
-   }
-   catch(SocketException& e){
-      std::cout << "Exception was caught:" << e.description() << "\n";
-   }
+    try
+    {
+        // Replace "localhost" with the hostname
+        // that you're running your server.
+        ClientSocket client_socket("localhost", 30000);
+        ClientSocket listening_socket("localhost", 31000);
+        std::string reply;
+        std::string formattedReply;
+        // Usually in real applications, the following
+        // will be put into a loop.
+        while (true)
+        {
+            try
+            {
+                //client_socket << "Test message.";
 
-   return 0;
+                listening_socket >> reply;
+                int n = reply.length();
+                std::string incoming;
+                std::string parity;
+                std::string ackNumber;
+                incoming = reply.substr(0, n-2);
+                formattedReply = incoming;
+                parity = reply.substr(n-1, 1);
+                ackNumber = reply.substr(n-2,1);
+                int parityCheck = checkParity(formattedReply);
+                std::cout << ackNumber;
+                if (std::to_string(parityCheck) == parity)
+                {
+                    client_socket << ackNumber;
+                }
+                else
+                {
+                    client_socket << "";
+                }
+                //client_socket << ackNumber;
+            }
+            catch(SocketException&)
+            {
+            }
+            std::cout << "We received this response from the server:\n\"" << formattedReply << "\"\n";
+        }
+    }
+    catch(SocketException& e)
+    {
+        std::cout << "Exception was caught:" << e.description() << "\n";
+    }
+
+    return 0;
 }
--- 3780/server.cpp
+++ 3780/server.cpp
@@ -8,16 +8,20 @@
 #include <cstdlib>
 #include <ctime>
 
-int checkParity(std::string incoming) {
+
+int checkParity(std::string incoming)
+{
     std::string parityCheck;
     parityCheck = incoming;
     int currentCount = 0, temp;
-    for (int i = 0; i < parityCheck.length(); i++) {
+    for (int i = 0; i < parityCheck.length(); i++)
+    {
         char tmp = parityCheck[i];
         int translation = static_cast<int>(tmp);
         currentCount += translation;
     }
-    while(currentCount != 0 ) {
+    while(currentCount != 0 )
+    {
         temp = currentCount % 2;
         currentCount /= 2;
     }
@@ -27,84 +31,96 @@ int checkParity(std::string incoming) {
         return 0;
 }
 
-int getSeqNum(int current) {
+std::string getSeqNum(int current)
+{
     int c = current;
     if ((c%2) == 0)
-        return 0;
+        return "0";
     else
-        return 1;
+        return "1";
 }
 
-void seedRandomGenerator() {
+void seedRandomGenerator()
+{
     srand(time(0));
 }
 
 int main(int argc, int argv[])
 {
-   std::cout << "running..\n";
-   try{
-      // Create the socket
-      ServerSocket server(30000);
+    std::cout << "running..\n";
+    try
+    {
+        // Create the socket
+        ServerSocket server(30000);
+        ServerSocket sender(31000);
 
-      while (true){
-	 ServerSocket new_sock;
-	 server.accept(new_sock);
+        while (true)
+        {
+            ServerSocket listening_sock;
+            server.accept(listening_sock);
 
-     //ServerSocket data_sock;
-     //server.accept(data_sock);
-
-
-     std::vector<std::string> toSend;
-     std::string currentLine;
-     std::ifstream infile;
-     infile.open("s1.txt");
-     while(std::getline(infile, currentLine, '.')) {
-        toSend.push_back(currentLine);
-     }
-     bool canSend = true;
-     int counter = 0;
-     int seqNum = 0;
-     int parity_bit;
-     seedRandomGenerator();
+            ServerSocket data_sock;
+            sender.accept(data_sock);
 
 
-	 // For multiple threading, you need to create
-	 // a new thread here and pass new_sock to it.
-	 // The thread will use new_sock to communicate
-	 // with the client.
-	 try{
-	    while (true){
-	       std::string data;
-	       if (canSend) {
-            std::string sending = toSend[counter];
-            seqNum = getSeqNum(counter);
-            sending.append(std::to_string(seqNum));
-            parity_bit = checkParity(toSend[counter]);
-            int rng = rand() % 5;
-            if (rng = 2)
-                parity_bit = (parity_bit + 1) % 2;
-            sending.append(std::to_string(parity_bit));
-            new_sock << sending;
-	       }
-	       new_sock >> data;
-	       std::string reply = data;
-	       if ((reply == std::to_string(seqNum)) == 0) {
-                canSend = true;
-                counter ++;
-	       }
-	       else {
-            canSend = true;
-	       }
-	       continue;
-	       //new_sock << data;
-	    }
-	 }
-	 catch(SocketException&){
-	 }
-      }
-   }
-   catch (SocketException& e){
-      std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
-   }
-  return 0;
+            std::vector<std::string> toSend;
+            std::string currentLine;
+            std::ifstream infile;
+            infile.open("s1.txt");
+            while(std::getline(infile, currentLine, '.'))
+            {
+                toSend.push_back(currentLine);
+            }
+            bool canSend = true;
+            int counter = 0;
+            std::string seqNum;
+            int parity_bit;
+            seedRandomGenerator();
+
+
+            // For multiple threading, you need to create
+            // a new thread here and pass new_sock to it.
+            // The thread will use new_sock to communicate
+            // with the client.
+            try
+            {
+                while (true)
+                {
+                    std::string data;
+                    if (canSend)
+                    {
+                        std::string sending = toSend[counter];
+                        seqNum = getSeqNum(counter);
+                        sending.append(seqNum);
+                        parity_bit = checkParity(toSend[counter]);
+                        int rng = rand() % 5;
+                        if (rng = 1)
+                            parity_bit = (parity_bit + 1) % 2;
+                        sending.append(std::to_string(parity_bit));
+                        data_sock << sending;
+                    }
+                    listening_sock >> data;
+                    std::string reply = data;
+                    if (reply == seqNum)
+                    {
+                        canSend = true;
+                        counter++;
+                    }
+                    else
+                    {
+                        canSend = true;
+                    }
+                    //new_sock << data;
+                }
+            }
+            catch(SocketException&)
+            {
+            }
+        }
+    }
+    catch (SocketException& e)
+    {
+        std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
+    }
+    return 0;
 }
