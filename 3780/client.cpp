e5adb0b09b6106ad0c9db1100f61a9d0cc5339a2
btk-dev <brandon.kuss@mymhc.ca>
2018-12-03 18:07:34 -0700

    Update client.cpp

--- 3780/client.cpp
+++ 3780/client.cpp
@@ -62,7 +62,6 @@ int main(int argc, int argv[])
                 ackNumber = reply.substr(n-2,1);
                 std::string parityCheck;
                 parityCheck = checkParity(formattedReply);
-                std::cout << parity << " " << parityCheck << " " << ackNumber << " " << reply;
                 if (parityCheck == parity)
                 {
                     std::string ackReply = ackNumber;
